abd3bd8b50f5895b0763e6e0a5535bf5bdfa715a
mirage335 <spamfreemirage335@gmail.com>
2015-10-07 23:16:58 -0400

    Inclusion of /usr/share/applications/kde4 directory, iconShortcut script documentation.

--- disableSystemMenu.sh
+++ disableSystemMenu.sh
@@ -9,10 +9,18 @@ mkdir -p /usr/share/applications
 mkdir -p /usr/share/applications.bak
 mv /usr/share/applications/* /usr/share/applications.bak/
 
+mkdir -p /usr/share/applications/kde4
+mkdir -p /usr/share/applications.bak/kde4
+mv /usr/share/applications/kde4/* /usr/share/applications.bak/kde4/
+
 
 mkdir -p /usr/local/share/applications
 mkdir -p /usr/local/share/applications.bak
 mv /usr/local/share/applications/* /usr/local/share/applications.bak/
+
+mkdir -p /usr/share/desktop-directories
+mkdir -p /usr/share/desktop-directories.bak
+mv /usr/share/desktop-directories/* /usr/share/desktop-directories.bak/
 
 
 mkdir -p /etc/xdg
